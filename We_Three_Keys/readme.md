fa0e17677204bb60b7216f2fb0f78f7e24729ad4
Kang In Park <kip218@nyu.edu>
2019-04-11 17:57:08 -0400

    We_Three_Keys

---
+++ We_Three_Keys/readme.md
@@ -0,0 +1,17 @@
+# CTF-name year : challenge-name 
+
+**Category:**  
+**Points:**  
+**Author:**  
+**Description:**  
+>
+>
+>
+
+## Write-up
+
+### Challenge explanation
+
+
+### Challenge solution
+
---
+++ We_Three_Keys/three_keys.py
@@ -0,0 +1,26 @@
+from pwn import *
+
+
+def get_decrypted_msg(key):
+    r = remote('chal1.swampctf.com', 1441)
+    r.recvuntil('Pick your key, and pick wisely!\n')
+    r.sendline(str(key))
+    r.recv()
+    r.sendline('2')
+    r.recvuntil('What message would you like to decrypt (in hex)?\n')
+    r.sendline('0' * 16 * 2 * 3)  # Three blocks of plaintext in hex
+    r.recvuntil('<=')
+    decrypted_msg = r.recvline().strip()
+    r.close()
+    return str(decrypted_msg, 'utf-8')
+
+
+def get_flag(key):
+    decrypted_msg = get_decrypted_msg(key)
+    plaintext1 = decrypted_msg[:32]
+    plaintext3 = decrypted_msg[64:]
+    flag = int(plaintext1, 16) ^ int(plaintext3, 16)
+    return bytes.fromhex(hex(flag)[2:])
+
+
+print(get_flag(1) + get_flag(2) + get_flag(3))
