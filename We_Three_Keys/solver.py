0fd85414657f8118f1b7047ea7952a651f684d99
Kang In Park <kip218@nyu.edu>
2019-04-14 03:07:04 -0400

    commit

--- We_Three_Keys/solver.py
+++ We_Three_Keys/solver.py
@@ -8,7 +8,7 @@ def get_decrypted_msg(key):
     r.recv()
     r.sendline('2')
     r.recvuntil('What message would you like to decrypt (in hex)?\n')
-    r.sendline('0' * 16 * 2 * 3)  # Three blocks of plaintext in hex
+    r.sendline('0' * 16 * 2 * 2)  # Two blocks of plaintext in hex
     r.recvuntil('<=')
     decrypted_msg = r.recvline().strip()
     r.close()
@@ -18,8 +18,8 @@ def get_decrypted_msg(key):
 def get_flag(key):
     decrypted_msg = get_decrypted_msg(key)
     plaintext1 = decrypted_msg[:32]
-    plaintext3 = decrypted_msg[64:]
-    flag = int(plaintext1, 16) ^ int(plaintext3, 16)
+    plaintext2 = decrypted_msg[32:]
+    flag = int(plaintext1, 16) ^ int(plaintext2, 16)
     return bytes.fromhex(hex(flag)[2:])
 
 
